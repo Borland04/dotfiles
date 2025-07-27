d7dc103f45391e852395aef5ec885a5ebed08446
Borland <McGarret160497@gmail.com>
2025-07-27 16:53:01 +0300

    Close neotree when it's the last window

--- .config/nvim/lua/custom/plugins/filetree.lua
+++ .config/nvim/lua/custom/plugins/filetree.lua
@@ -7,8 +7,11 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		require("neo-tree").setup({})
+		require("neo-tree").setup({
+			close_if_last_window = true,
+		})
 
 		vim.keymap.set("n", "<leader>n", ":Neotree toggle reveal<cr>", { desc = "Toggle filesystem [N]eoTree" })
+		vim.keymap.set('n', '<c-n>', ':Neotree toggle reveal<cr>', { desc = 'Toggle filesystem [N]eoTree' })
 	end,
 }
